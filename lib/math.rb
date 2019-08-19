48adbf2aa796728fbe63d83d7549257fcf3030b4
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-19 19:10:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ return sum
 end
 
 def subtraction(num1, num2)
-
+diff = num1 - num2
+return diff
 end
 
 def division(num1, num2)
-
+quot = num1 / num2
+return quot
 end
 
 def multiplication(num1, num2)
-
+prod = num1 * num2
+return prod
 end
 
 def modulo(num1, num2)
